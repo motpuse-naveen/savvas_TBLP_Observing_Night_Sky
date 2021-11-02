5c09d874d3e3088745f7c7381a697d18a2cbfca2
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2021-11-02 22:09:04 +0530

    Some accessibility changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     document.addEventListener('click', function (e) {
         $('body').removeClass('show-focus-outlines');
     });
-    setTimeout(function(){
+    setTimeout(function () {
         $(".annim1").removeClass("annim1")
         $(".annim2").removeClass("annim2")
         $(".annim3").removeClass("annim3")
@@ -30,7 +30,7 @@ $(document).ready(function () {
         $(".annim4-333").removeClass("annim4-333")
         $(".annim4-444").removeClass("annim4-444")
         $(".annim4-555").removeClass("annim4-555")
-    },3000)
+    }, 3000)
 });
 
 $(document).on('click', '.link-learmore', function (event) {
@@ -52,57 +52,78 @@ $(document).on('click', '#btn-discussiontip', function (event) {
 });
 
 $(document).on('click', '#discussiontipClose', function (event) {
-        $(".container-fs").ShowElement();
-        $("#btn-discussiontip").focus();
-        $('.container-fs-popup.discussiontip').HideElement();
+    $(".container-fs").ShowElement();
+    $("#btn-discussiontip").focus();
+    $('.container-fs-popup.discussiontip').HideElement();
 });
-$(document).on("click", "#close-discussiontip", function(event){
+$(document).on("click", "#close-discussiontip", function (event) {
     $(".container-fs").ShowElement();
     $("#btn-discussiontip").focus();
     $('.container-fs-popup.discussiontip').HideElement();
 })
 
-$(document).on("click", '.link-tab', function(event){
-    if(!$(this).hasClass("active")){
+$(document).on("click", '.link-tab', function (event) {
+    if (!$(this).hasClass("active")) {
         var dataid = $(this).attr("data-tabid");
-        $(".link-tab.active").removeClass("active").attr("aria-selected","false")
-        $(this).addClass("active").attr("aria-selected","true")
+        $(".link-tab.active").removeClass("active").attr("aria-selected", "false")
+        $(this).addClass("active").attr("aria-selected", "true")
         $("#" + dataid).find(".side-nav li a").removeClass("active").attr("aria-current", "false").removeAttr("aria-describedby");
         $("#" + dataid).ActiveTabs();
         $("#" + dataid).find(".side-nav li a:first").focus();
-        $(".container-fs.slide:not(#" + dataid +")").InactiveTabs();
+        $(".container-fs.slide:not(#" + dataid + ")").InactiveTabs();
     }
 });
 
-$(document).on("click", ".side-nav-circle ul li a", function(event){
+$(document).on("click", ".side-nav-circle ul li a", function (event) {
     $(".side-nav-circle ul li a").removeClass("active");
     $(this).addClass("active");
 })
 
-$(document).on("click", ".card-body .card-link", function(event){
+$(document).on("click", ".card-body .card-link", function (event) {
     var tablinkid = $(this).attr("data-tablinkid");
-    $("#" + tablinkid).trigger( "click" );
+    $("#" + tablinkid).trigger("click");
 })
-$(document).on("click", "#linkNameTheFullMoon,#linkStudentTextPreview", function(event){
+$(document).on("click", "#linkNameTheFullMoon,#linkStudentTextPreview", function (event) {
     $('.container-fs-popup.fullmoon').ShowElement();
     $("#fullmoonClose").focus();
     $(".container-fs").HideElement();
 })
 
-$(document).on("click", "#close-fullmoonimage", function(event){
+$(document).on("click", "#close-fullmoonimage", function (event) {
     $(".container-fs").ShowElement();
     $("#linkNameTheFullMoon").focus();
     $('.container-fs-popup.fullmoon').HideElement();
 })
-$(document).on("click", "#fullmoonClose", function(event){
+$(document).on("click", "#fullmoonClose", function (event) {
     $(".container-fs").ShowElement();
     $("#linkNameTheFullMoon").focus();
     $('.container-fs-popup.fullmoon').HideElement();
 })
 
 $(document).on('keypress', '#fullmoonClose, #close-fullmoonimage, #close-discussiontip, #discussiontipClose, #disclaimerClose', function (event) {
-    if(event.which == 13 || event.which == 32) {
+    if (event.which == 13 || event.which == 32) {
         $(this).click();
+    }
+});
+
+$(document).on("click", ".card-button", function (event) {
+
+});
+
+$(document).on('keypress', ".card-button", function (event) {
+    if (event.which == 13 || event.which == 32) {
+        //$(this).click();
+        var backVisible = $(this).hasClass("showback")
+        if (backVisible) {
+            $(this).removeClass("showback").attr("aria-expanded", "false")
+            $("#livecontent").text("")
+        }
+        else {
+            $(this).addClass("showback").attr("aria-expanded", "true")
+            $("#livecontent").text($(this).find(".card-back").text());
+        }
+        event.preventDefault();
+        event.stopPropagation();
     }
 });
 
